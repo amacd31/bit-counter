27ac7c98699e6d7ddf9ff03525f765414d51c483
Andrew MacDonald <andrew@maccas.net>
2022-07-09 14:05:07 +1000

    Implement `count_ones` for np.array of uint8
    Basic count_ones method for counting up the ones in a numpy uint8 (i.e.
    a `np.packbits` array).

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,32 @@
-
+use bitvec::prelude::*;
+use numpy::PyReadonlyArrayDyn;
+use pyo3::exceptions::PyTypeError;
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
+/// count_ones(arr)
+/// --
+///
+/// Count the number of ones in a numpy array of uint8.
+///
+/// Used for counting up the number of one values in a `uint8`
+/// `np.array`. The input to this method would typically be the
+/// the output of `np.packbits`.
+///
+/// :param arr: Numpy uint8 array to count the number of one bits in.
+/// :returns: Integer of the number of bits that were one in the array.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn count_ones(arr: PyReadonlyArrayDyn<u8>) -> PyResult<usize> {
+    let arr = match arr.as_slice() {
+        Ok(arr) => arr,
+        Err(error) => return Err(PyTypeError::new_err(error)),
+    };
+
+    let bits = BitSlice::<_, Lsb0>::from_slice(&arr);
+    Ok(bits.count_ones())
 }
 
-/// A Python module implemented in Rust.
 #[pymodule]
 fn bit_counter(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(count_ones, m)?)?;
     Ok(())
 }
